510275d2b23f58681fe01ff80438abd99dfb767c
bodanor <christos.papadopoulos@student.hepl.be>
2022-08-18 22:29:32 +0200

    Added headers files

--- hc-sr04.h
+++ hc-sr04.h
@@ -2,5 +2,9 @@
 #define __HC_SR04_H__
 
 #include <avr/io.h>
+#include "atmega328p-lib/include/uart.h"
+#include "atmega328p-lib/include/timer.h"
+#include "atmega328p-lib/include/util_pin.h"
+
 
 #endif
